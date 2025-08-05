34a0ee910c401e0c33366475d5d488383da7bd85
Amaan127001 <amaankhan2489@gmail.com>
2025-08-05 13:21:03 +0530

    deployment update

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -4,18 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthContext';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
 
